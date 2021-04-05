36c8ff8904d3a59930c9be801535c4adfffded8c
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2021-04-05 19:57:57 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Home} />
+    </BrowserRouter>
+  )
+}
+
+export default App
